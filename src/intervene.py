ca80a804077c25329ef6acb177a1b8efc23ca8af
bejīta <134194735+onlybejita@users.noreply.github.com>
2023-07-04 08:45:02 +1000

    Update intervene.py
    Check to ensure that word_index and sense_index are within the valid range, and that new_vector has the same size as the sense vectors.

--- src/intervene.py
+++ src/intervene.py
@@ -1,6 +1,18 @@
 from .model import Backpack
 
 def intervene(model, word_index, sense_index=None, new_vector=None, suppress=False):
+    # Check if word_index is valid
+    if word_index < 0 or word_index >= model.sense_vectors.size(0):
+        raise ValueError(f"word_index must be between 0 and {model.sense_vectors.size(0)-1}")
+
+    # Check if sense_index is valid
+    if sense_index is not None and (sense_index < 0 or sense_index >= model.sense_vectors.size(1)):
+        raise ValueError(f"sense_index must be between 0 and {model.sense_vectors.size(1)-1}")
+
+    # Check if new_vector is valid
+    if new_vector is not None and new_vector.size() != model.sense_vectors[0, 0].size():
+        raise ValueError(f"new_vector must have the same size as the sense vectors")
+
     if suppress:
         if sense_index is None:
             raise ValueError("sense_index must be provided when suppress is True")
